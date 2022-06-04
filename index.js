e261dfdde7bb0e04db8cd87d3b26f1ab5d3d9151
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-04 15:33:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const thankYouCards=[]
+function writeCards(namesArray, event){
+    for (let i=0; i< namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+return thankYouCards;
+}
+function countDown (startingNumber){
+    while (startingNumber > 0){
+        console.log( startingNumber );
+        startingNumber -= 1;
+    }
+    console.log( startingNumber );
+
+}
